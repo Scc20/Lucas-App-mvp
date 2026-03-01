c8c998ae98712df7689af9cc79e1f6020e230b87
Scc20 <solucionesconcalidad2.0@gmail.com>
2026-02-28 20:19:22 -0600

    Delete src/utils/Icon.jsx

--- src/utils/Icon.jsx
+++
@@ -1,9 +0,0 @@
-import * as Icons from "lucide-react";
-
-const Icon = ({ name, size = 24, className = "", strokeWidth = 2 }) => {
-  const LucideIcon = Icons[name];
-  if (!LucideIcon) return null;
-  return <LucideIcon size={size} className={className} strokeWidth={strokeWidth} />;
-};
-
-export default Icon;
